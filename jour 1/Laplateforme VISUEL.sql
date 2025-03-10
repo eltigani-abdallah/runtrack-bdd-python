a535d2f9d2e92c006de0b8d2d31ff1702a4cd5c3
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-03-10 14:31:32 +0100

    feat: créer base 'laplateforme' et base laplateforme VISUEL'

---
+++ jour 1/Laplateforme VISUEL.sql
@@ -0,0 +1,11 @@
+ select * from etudiant;
++----+-----------+----------+-----+---------------------------------+
+| id | nom       | prenom   | age | email                           |
++----+-----------+----------+-----+---------------------------------+
+|  1 | spaghetti | betty    |  23 | betty.spaghetti@laplateforme.io |
+|  2 | steak     | chuck    |  45 | chuck.steak@laplateforme.io     |
+|  3 | doe       | john     |  18 | john.doe@laplateforme.io        |
+|  4 | barnes    | binkie   |  16 | binkie.barnes@laplateforme.io   |
+|  5 | dupuis    | gertrude |  20 | gertrude.dupuis@laplateforme.io |
++----+-----------+----------+-----+---------------------------------+
+5 rows in set (0.00 sec)
