646c75f4f6d792e4428a11832a1111c10a37104d
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-03-10 15:11:40 +0100

    feat: jour 1 job 12

---
+++ jour 1/job12.sql
@@ -0,0 +1,8 @@
+mysql> select * from etudiant where nom="dupuis";
++----+--------+----------+-----+---------------------------------+
+| id | nom    | prenom   | age | email                           |
++----+--------+----------+-----+---------------------------------+
+|  5 | dupuis | gertrude |  20 | gertrude.dupuis@laplateforme.io |
+|  6 | dupuis | martin   |  18 | martin.dupuis@laplateforme.io   |
++----+--------+----------+-----+---------------------------------+
+2 rows in set (0.00 sec)
