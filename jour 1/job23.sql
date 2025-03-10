f82e871f51eb4f704a42d495eb60951ade2a0444
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-03-10 15:57:43 +0100

    feat: jour1 job 23

---
+++ jour 1/job23.sql
@@ -0,0 +1,7 @@
+mysql> select * from etudiant order by age desc limit 1;
++----+-------+--------+-----+-----------------------------+
+| id | nom   | prenom | age | email                       |
++----+-------+--------+-----+-----------------------------+
+|  2 | steak | chuck  |  45 | chuck.steak@laplateforme.io |
++----+-------+--------+-----+-----------------------------+
+1 row in set (0.00 sec)
