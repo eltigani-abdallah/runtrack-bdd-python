cfe91d2c9e935c0509b5a2b8b6fbc53257f6cc98
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-03-10 15:56:43 +0100

    feat: jour1 job 22

---
+++ jour 1/job22.sql
@@ -0,0 +1,7 @@
+mysql> select * from etudiant order by age limit 1;
++----+--------+--------+-----+-------------------------------+
+| id | nom    | prenom | age | email                         |
++----+--------+--------+-----+-------------------------------+
+|  4 | barnes | binkie |  16 | binkie.barnes@laplateforme.io |
++----+--------+--------+-----+-------------------------------+
+1 row in set (0.00 sec)
